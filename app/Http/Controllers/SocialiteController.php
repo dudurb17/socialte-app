b551b4a11c8071594c393f933b0c84456fd4c94e
dudurb17 <eduardo.robettibedin@gmail.com>
2025-01-08 16:17:51 -0300

    finished login with google

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialiteController extends Controller
@@ -16,8 +19,28 @@ class SocialiteController extends Controller
     }
     public function googleAuthentication()
     {
+        try {
+            $googleUser = Socialite::driver('google')->user();
+            $user = User::where('google_id', $googleUser->id)->first();
+            if ($user) {
+                Auth::login($user);
+                return redirect()->route('dashboard');
+            } else {
+                $userData = User::create([
+                    'name' => $googleUser->name,
+                    'email' => $googleUser->email,
+                    'password' => Hash::make('password@1234'),
+                    'google_id' => $googleUser->id
+                ]);
+                if ($userData) {
+                    Auth::login($userData);
+                    return redirect()->route('dashboard');
 
-        $googleUser = Socialite::driver('google')->user();
-        dd($googleUser);
+                }
+            }
+        } catch (\Throwable $th) {
+            dd(vars: $th);
+        }
+
     }
 }
